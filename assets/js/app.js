a11c218cc4f3120be1d3c6b702706a108d55d9dd
anurag-512 <anuragsharan51@gmail.com>
2024-04-02 11:24:50 +0530

    slider done

---
+++ assets/js/app.js
@@ -0,0 +1,38 @@
+$('.sliderbox').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    nextArrow: ".next",
+    prevArrow: ".prev",
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    infinite: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 2000,
+            }
+        }
+    ]
+});
